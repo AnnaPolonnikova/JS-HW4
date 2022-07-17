14d08254fdca0f50c0cfe6550d42624913751cb6
Anna <Anna.polonnikova@list.ru>
2022-07-17 13:49:43 +0300

    commit2

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,4 +1,10 @@
+// Задание1
  let arr = [1, 2, 3, 4, 5];
-for (let i = 0; i< arr.length; i++){
+ for (let i = 0; i< arr.length; i++){
 document.write(arr[i]);
-}
+ }
+// Задание2
+ let arr2 = [ -2, -1, -3, 15, 0, -4, 2, -5, 9, -15, 0, 4, 5, -6, 10, 7];
+ for (i = 0; i > -10 && i < -3;i++ );{
+    document.write(arr2[i])
+ }
